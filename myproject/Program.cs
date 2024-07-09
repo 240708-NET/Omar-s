47d85421aaa4f64841dcda8ef4f29143bb6ae136
ommarico2 <ommarico1@gmail.com>
2024-07-09 14:13:55 -0500

    Update

---
+++ myproject/Program.cs
@@ -0,0 +1,27 @@
+public class Program{
+
+	static void Main(string[] args){
+        
+	Console.WriteLine("Hello, World!");
+
+	Console.WriteLine("Please enter your name : ");
+	
+	string username = "Omar";
+
+	username = "something";
+
+	username = Console.ReadLine();
+
+	//Console.WriteLine("Welcome to Revature: ");
+
+	//Console.WriteLine( username );
+
+	//Console.WriteLine("Welcome to Revature: " + username);
+	
+	//Console.WriteLine("Welcome to Revature: {0}" , username );
+
+	Console.WriteLine($"Welcome to Revature: {username}");
+  }
+
+
+}
